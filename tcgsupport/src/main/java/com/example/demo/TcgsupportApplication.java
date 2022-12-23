bca6ecd628f0e11d21f8ac36551ec886895c7356
nishino <nishino@asojuku.ac.jp>
2022-12-23 17:45:23 +0900

    12/23

---
+++ tcgsupport/src/main/java/com/example/demo/TcgsupportApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcgsupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcgsupportApplication.class, args);
+	}
+
+}
